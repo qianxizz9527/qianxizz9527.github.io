05ecd45bd0e48620ca2d5928b0c8fdbb6ddeb533
zzrlc <2467254599@qq.com>
2022-04-26 17:56:45 +0800

    ..

--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -1,21 +1,11 @@
 <template>
   <div class="home">
-    <!-- container -->
-    <section>
-      1
-    </section>
-    <!-- picture -->
-    <section>
-      2
-    </section>
-    <!-- footer -->
-    <section>
-      3
-    </section>
   </div>
 </template>
 
 <script>
+// 方式 1: 导入整个 three.js核心库
+import * as THREE from 'three';
 
 export default {
   components: {},
@@ -24,6 +14,32 @@ export default {
     return {
       home: { },
     };
+  },
+  created() {
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setSize( window.innerWidth, window.innerHeight )
+    document.body.appendChild( renderer.domElement )
+
+    const geometry = new THREE.BoxGeometry()
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
+    const cube = new THREE.Mesh( geometry, material )
+    scene.add( cube )
+
+    camera.position.z = 5
+
+    function animate() {
+      requestAnimationFrame( animate )
+
+      cube.rotation.x += 0.01
+      cube.rotation.y += 0.01
+
+      renderer.render( scene, camera )
+    }
+
+    animate();
   },
 
   methods: {
